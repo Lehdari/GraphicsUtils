8ae563981ce14f4ab4d3af510e78085d0a23fa55
Lehdari <miika.lehtimaki@gmail.com>
2019-05-26 01:14:54 +0300

    main.cpp: Load bunny

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -5,8 +5,13 @@
 #include "OpenGLUtils/App.hpp"
 
 
-int main(int argv, char** args) {
+int main(int argv, char** args)
+{
     App app;
+
+    Mesh bunny;
+    bunny.loadFromObj(std::string(RES_PATH) + "models/bunny.obj");
+    app.addMesh(std::move(bunny));
     app.loop();
 
     return 0;
