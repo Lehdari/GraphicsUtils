cf94e56c75c9bb0aa2926e1a58c06edc3c41089c
Lehdari <miika.lehtimaki@gmail.com>
2020-03-27 17:45:48 +0200

    Add gut_image library target

---
+++ include/gut_image/Image.hpp
@@ -0,0 +1,24 @@
+//
+// Project: GraphicsUtils
+// File: Image.hpp
+//
+// Copyright (c) 2020 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef GRAPHICSUTILS_IMAGE_HPP
+#define GRAPHICSUTILS_IMAGE_HPP
+
+
+namespace gut {
+
+    class Image {
+
+    };
+
+} // namespace gut
+
+
+#endif //GRAPHICSUTILS_IMAGE_HPP
