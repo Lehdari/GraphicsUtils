c264b4c542143adb5af9f8bfff5e76e32f42f894
Lehdari <miika.lehtimaki@gmail.com>
2020-03-27 17:45:48 +0200

    Add gut_image library target

---
+++ src/gut_image/Image.cpp
@@ -0,0 +1,12 @@
+//
+// Project: GraphicsUtils
+// File: Image.cpp
+//
+// Copyright (c) 2020 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "Image.hpp"
+
