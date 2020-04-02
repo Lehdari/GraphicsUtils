6bb9a92a1c36afd753de5b9976b7a3098262eeea
Lehdari <miika.lehtimaki@gmail.com>
2020-04-02 16:20:14 +0300

    Camera: Realign matrix formations in lookAt() and projection()

--- src/OpenGLUtils/Camera.cpp
+++ src/OpenGLUtils/Camera.cpp
@@ -41,7 +41,8 @@ void Camera::lookAt(const Vec3f& eye, const Vec3f& target, const Vec3f& up) {
     Vec3f xAxis = up.cross(zAxis).normalized();
     Vec3f yAxis = zAxis.cross(xAxis);
 
-    _worldToCamera << xAxis[0], xAxis[1], xAxis[2], -xAxis.dot(eye),
+    _worldToCamera <<
+        xAxis[0], xAxis[1], xAxis[2], -xAxis.dot(eye),
         yAxis[0], yAxis[1], yAxis[2], -yAxis.dot(eye),
         zAxis[0], zAxis[1], zAxis[2], -zAxis.dot(eye),
         0.0f    , 0.0f    , 0.0f    , 1.0f           ;
@@ -51,7 +52,8 @@ void Camera::projection(float fov, float aspectRatio, float near, float far) {
     float r = tanf(fov / 2.0f) * near;
     float t = r / aspectRatio;
 
-    _cameraToClip << near/r, 0.0f  , 0.0f                  , 0.0f                     ,
+    _cameraToClip <<
+        near/r, 0.0f  , 0.0f                  , 0.0f                     ,
         0.0f  , near/t, 0.0f                  , 0.0f                     ,
         0.0f  , 0.0f  , -(far+near)/(far-near), -2.0f*far*near/(far-near),
         0.0f  , 0.0f  , -1.0f                 , 0.0f                     ;
