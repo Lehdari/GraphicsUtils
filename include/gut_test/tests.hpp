17a63bc3285febd620ffa800e7811ca7b9e0b2a3
Lehdari <miika.lehtimaki@gmail.com>
2020-04-03 22:09:29 +0300

    gut_test: Add separate test sources

---
+++ include/gut_test/tests.hpp
@@ -0,0 +1,23 @@
+//
+// Project: GraphicsUtils
+// File: tests.hpp
+//
+// Copyright (c) 2020 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef GRAPHICSUTILS_TESTS_HPP
+#define GRAPHICSUTILS_TESTS_HPP
+
+
+namespace gut {
+
+    int testOpenGL();
+    int testImage();
+
+} // namespace gut
+
+
+#endif //GRAPHICSUTILS_TESTS_HPP
