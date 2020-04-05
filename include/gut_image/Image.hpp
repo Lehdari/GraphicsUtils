67eba1197cf3e6526d8b6caee36e49a2159da17f
Lehdari <miika.lehtimaki@gmail.com>
2020-04-05 21:33:29 +0300

    Image: Add documentation

--- include/gut_image/Image.hpp
+++ include/gut_image/Image.hpp
@@ -19,19 +19,26 @@
 
 namespace gut {
 
+    /** @brief  Image class for generic 2D image data storage and I/O
+     */
     class Image {
     public:
+        /** @brief  Supported image pixel data formats, specifies number and order of channels
+         */
         enum class DataFormat {
             GRAY,
             RGB,
             RGBA
         };
 
+        /** @brief  Supported image data types
+         *  @note   Matching value type is to be used in template interfaces
+         */
         enum class DataType {
             INVALID,
-            U8,
-            U16,
-            F32
+            U8,     // uint8_t
+            U16,    // uint16_t
+            F32     // float
         };
 
         inline constexpr static int nChannels(DataFormat dataFormat);
@@ -41,6 +48,10 @@ namespace gut {
 
         struct PixelRef;
 
+        /** @brief  Pixel struct to be used to modify image data
+         *  @tparam T_Data  Data type of the pixel; must match the Image data type
+         *  @note   Most pixel operations do not involve safety checks to enable maximum performance
+         */
         template <typename T_Data>
         struct Pixel {
             T_Data  r;
@@ -48,12 +59,20 @@ namespace gut {
             T_Data  b;
             T_Data  a;
 
+            /** @brief  Construct a pixel object
+             *  @param  r   Red channel value
+             *  @param  g   Green channel value
+             *  @param  b   Blue channel value
+             *  @param  a   Alpha channel value
+             */
             Pixel(T_Data r, T_Data g, T_Data b, T_Data a);
 
             Pixel(const PixelRef& pRef);
             Pixel<T_Data>& operator=(const PixelRef& pRef);
         };
 
+        /** @brief  Pixel reference struct, used as a proxy object when accessing image data
+         */
         struct PixelRef {
         private:
             void*       data;
@@ -77,6 +96,10 @@ namespace gut {
             PixelRef operator++(int);
         };
 
+        /** @brief  Construct an Image object
+         *  @param  dataFormat  Pixel data format (number and order of channels)
+         *  @param  dataType    Pixel data type (precision)
+         */
         explicit Image(
             DataFormat dataFormat   = DataFormat::RGB,
             DataType dataType       = DataType::U8);
@@ -110,14 +133,38 @@ namespace gut {
          */
         void writeToFile(const std::string& fileName);
 
+        /** @brief  Get pixel data format of the image
+         *  @return Pixel data format of the image
+         */
         DataFormat dataFormat() const noexcept;
+
+        /** @brief  Get data type of the image
+         *  @return Data type of the image
+         */
         DataType dataType() const noexcept;
 
+        /** @brief  Get width of the image
+         *  @return Width of the image in pixels
+         */
         int width() const noexcept;
+        /** @brief  Get  of the image
+         *  @return Height of the image in pixels
+         */
         int height() const noexcept;
 
+        /** @brief  Access a pixel at location
+         *  @param  x   x-coordinate of the pixel to be accessed
+         *  @param  y   y-coordinate of the pixel to be accessed
+         *  @return Pixel at given location
+         *  @note   This operator does not perform boundary checks to allow for maximum performance
+         */
         PixelRef operator()(int x, int y);
 
+        /** @brief  Set a pixel at location
+         *  @param  x   x-coordinate of the pixel to be set
+         *  @param  y   y-coordinate of the pixel to be set
+         *  @note   This operator does not perform boundary or type checks to allow for maximum performance
+         */
         template <typename T_Data>
         void setPixel(int x, int y, const Pixel<T_Data>& p);
 
@@ -125,6 +172,7 @@ namespace gut {
          *  @tparam T_Data  Image data type (uint8_t, uint16_t or float)
          *  @return Read-only pointer to raw image data
          *  @note   The function performs type checking (use dataType() to check)
+         *  @note   Data is arranged in row-major order with pixels ordered as specified by the pixel data format
          */
         template <typename T_Data>
         T_Data* data();
@@ -133,6 +181,7 @@ namespace gut {
          *  @tparam T_Data  Image data type (uint8_t, uint16_t or float)
          *  @return Read-only pointer to raw image data
          *  @note   The function performs type checking (use dataType() to check)
+         *  @note   Data is arranged in row-major order with pixels ordered as specified by the pixel data format
          */
         template <typename T_Data>
         const T_Data* data() const noexcept;
