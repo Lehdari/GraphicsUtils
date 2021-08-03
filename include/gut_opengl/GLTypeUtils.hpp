a7804d56fbb3e8f9834c3f0797ca9c1c871e829d
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2021-08-03 16:45:03 +0300

    GLTypeUtils: Add imageDataFormatToGLEnum() and imageDataTypeToGLEnum()
    - Converter functions for image format and data type into OpenGL enumerations

--- include/gut_opengl/GLTypeUtils.hpp
+++ include/gut_opengl/GLTypeUtils.hpp
@@ -15,18 +15,32 @@
 #include <glad/glad.h>
 
 #include <gut_utils/MathTypes.hpp>
+#include <gut_image/Image.hpp>
 
 
 namespace gut {
 
-/** @brief  Get corresponding GL enumeration for a data type
- */
+    /** @brief  Get corresponding OpenGL enumeration for a data type
+     *  @tparam T_Data  Data type to get corresponding enumeration for
+     *  @return OpenGL enumeration corresponding to the data type
+     */
     template <typename T_Data>
     constexpr GLenum typeToGLEnum();
 
+    /** @brief  Get corresponding OpenGL enumeration for a image data format
+     *  @param  dataFormat  Data format to get the corresponding enumeration for
+     *  @return OpenGL enumeration corresponding to the image data format
+     */
+    GLenum imageDataFormatToGLEnum(Image::DataFormat dataFormat);
+
+    /** @brief  Get corresponding OpenGL enumeration for a image data type
+     *  @param  dataType    Data type to get the corresponding enumeration for
+     *  @return OpenGL enumeration corresponding to the image data type
+     */
+    GLenum imageDataTypeToGLEnum(Image::DataType dataType);
+
 
     #include "GLTypeUtils.inl"
-
 
 } // namespace gut
 
---
+++ src/gut_opengl/GLTypeUtils.cpp
@@ -0,0 +1,45 @@
+//
+// Project: evolution_simulator_2
+// File: GLTypeUtils.cpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENCE which is distributed
+// with this source code package.
+//
+
+#include "GLTypeUtils.hpp"
+
+
+using namespace gut;
+
+
+GLenum gut::imageDataFormatToGLEnum(Image::DataFormat dataFormat)
+{
+    switch (dataFormat) {
+        case Image::DataFormat::GRAY:
+            return GL_RED;
+        case Image::DataFormat::RGB:
+            return GL_RGB;
+        case Image::DataFormat::RGBA:
+            return GL_RGBA;
+    }
+
+    // should never be reached, maybe new data formats were added?
+    throw std::runtime_error("ERROR: imageDataFormatToGLEnum(): image data format to GLEnum conversion failed");
+}
+
+GLenum gut::imageDataTypeToGLEnum(Image::DataType dataType)
+{
+    switch (dataType) {
+        case Image::DataType::U8:
+            return GL_UNSIGNED_BYTE;
+        case Image::DataType::U16:
+            return GL_UNSIGNED_SHORT;
+        case Image::DataType::F32:
+            return GL_FLOAT;
+    }
+
+    // should never be reached, maybe new data types were added?
+    throw std::runtime_error("ERROR: imageDataTypeToGLEnum(): image data type to GLEnum conversion failed");
+}
