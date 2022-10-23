4cc7db37ad22d1c11e9fe9b38ddd788459b1441c
Lehdari <miika.lehtimaki@gmail.com>
2022-10-23 19:42:40 +0300

    gut_opengl: Add Node class

---
+++ include/gut_opengl/Node.hpp
@@ -0,0 +1,45 @@
+//
+// Project: GraphicsUtils
+// File: Node.hpp
+//
+// Copyright (c) 2022 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef GRAPHICSUTILS_NODE_HPP
+#define GRAPHICSUTILS_NODE_HPP
+
+
+#include <gut_utils/TypeUtils.hpp>
+#include <gut_utils/MathTypes.hpp>
+
+
+namespace gut {
+
+class Mesh;
+class Shader;
+class Camera;
+
+
+// Rendering graph node
+class Node {
+public:
+    Node(const Mat4f& t, int64_t meshId = -1);
+
+    // TODO refactor shader into material, and material into mesh primitive
+    void render(const Mat4f& tParent, const Vector<Mesh>& meshes, Shader& shader, const Camera& camera) const;
+
+    friend class GLTFLoader;
+
+private:
+    Mat4f           _t; // transformation relative to parent node
+    int64_t         _meshId;
+    Vector<Node>    _children;
+};
+
+} // namespace gut
+
+
+#endif //GRAPHICSUTILS_NODE_HPP
