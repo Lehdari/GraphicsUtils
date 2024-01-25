f08c5592fda359cc7cd75dc5d533011f0a49026a
Lehdari <miika.lehtimaki@gmail.com>
2024-01-25 14:15:20 +0200

    MathUtils: Make rnd() a function template

--- include/gut_utils/MathUtils.hpp
+++ include/gut_utils/MathUtils.hpp
@@ -34,13 +34,27 @@ inline Vec2f cubicInterpolate(const Vec2f (&p)[4], double x)
     return p[1] + 0.5 * x*(p[2] - p[0] + x*(2.0*p[0] - 5.0*p[1] + 4.0*p[2] - p[3] + x*(3.0*(p[1] - p[2]) + p[3] - p[0])));
 }
 
-inline float rnd(float min = 0.0f, float max = 1.0f)
+template <typename T>
+inline T rnd(T min = T(0), T max = T(1)) // [min, max)
+{
+    static std::default_random_engine r(time(NULL));
+    return min+static_cast<T>(r())%(max-min);
+}
+
+template <>
+inline float rnd<float>(float min, float max)
 {
     static std::default_random_engine r(time(NULL));
     return min+(max-min)*(r()%65536)*0.0000152590219f;
 }
 
-void fitPlane(Vec3f& p, Vec3f& n, const Vector<Vec3f>& points);
+template <>
+inline double rnd<double>(double min, double max)
+{
+    static std::default_random_engine r(time(NULL));
+    return min+(max-min)*(double)(r()%33554432ul)*2.98023223876953125e-8;
+}
+
 
 
 #endif //GRAPHICSUTILS_MATHUTILS_HPP
